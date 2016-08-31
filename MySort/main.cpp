6e91470ef2c8eae8e37ab72b770c971b19d47057
se7enkings <GT7_Liang@shiyiliangdemac-mini.local>
2016-09-01 01:50:30 +0800

    Second

---
+++ MySort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MySort
+//
+//  Created by ShiyiLiang on 16/9/1.
+//  Copyright © 2016年 ShiyiLiang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";//
+    return 0;
+}
