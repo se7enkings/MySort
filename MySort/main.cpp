c7cb2a9d6983a79f951646d61d098c0693b7f904
se7enkings <liangshiyi920105@hotmail.com>
2016-09-03 17:12:43 +0800

    ShellTest

--- MySort/main.cpp
+++ MySort/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include "ShellSort.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";//
-    return 0;
+	// insert code here...
+
+	int array[10] = { 9, 11, 6, 8, 75, 6, 3, 52, 44, 84 };
+	int array2[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int array3[10] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+	ShellSort(array, 10);
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d\t", array[i]);
+	}
+
+	while (true)
+	{
+	}
+	return 0;
 }
