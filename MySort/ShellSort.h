7472d392d8317a5e6b93fa417e8fae6ad2f751c2
se7enkings <liangshiyi920105@hotmail.com>
2016-09-03 17:11:11 +0800

    ShellSort

--- MySort/ShellSort.h
+++ MySort/ShellSort.h
@@ -3,7 +3,7 @@
 //  MySort
 //
 //  Created by ShiyiLiang on 16/9/1.
-//  Copyright 漏 2016骞?ShiyiLiang. All rights reserved.
+//  Copyright © 2016年 ShiyiLiang. All rights reserved.
 //
 
 #ifndef ShellSort_h
@@ -11,4 +11,28 @@
 
 #include <stdio.h>
 
-#endif /* ShellSort_hpp */
+void ShellSort(int array[], int len)
+{
+	int temp = 0, j = 0;
+
+	for (int div = len / 2; div >= 1; div /= 2)
+	{
+		for (int loop = 0; loop < div; loop++)//遍历各个组
+		{
+
+			for (int i = loop + div; i < len; i += div)//遍历当前组内的各个数
+			{
+				temp = array[i];
+				for (j = i; j >= div; j -= div) //j > div - 1 => j - div > -1 => j-div => 0
+				{
+					if (temp < array[j - div])
+						array[j] = array[j - div];
+					else
+						break;
+				}
+				array[j] = temp;//插入
+			}
+		}
+	}
+}
+#endif /* ShellSort_h */
